9ea35d801835b562b3510307b76b30f4552dff2c
kashifaxioned <kashifa@axioned.com>
2022-03-31 11:49:20 +0530

    JS functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,18 @@
 
 */
 
+let key = document.querySelector(".key-value") 
+let keyCode = document.querySelector(".key-code-value") 
+let code = document.querySelector(".code-value") 
+let pressSection = document.querySelector(".press-section")
+let eventsSection = document.querySelector(".events-section")  
 
+document.addEventListener("keydown", handleKey)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function handleKey(event) {
+  key.innerHTML = event.key;
+  keyCode.innerHTML = event.keyCode;
+  code.innerHTML = event.code;
+  pressSection.style.display = "none";
+  eventsSection.style.display = "flex";
+}
